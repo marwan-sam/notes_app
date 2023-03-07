7ff582880e57bda71c041e91eb0e29fe7f8de31f
marwan <marwan.elansarey@gmail.com>
2023-03-08 00:33:34 +0200

    change theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/screens/notes_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      // theme: ThemeData.light(),
+      theme: ThemeData.dark(),
+      home: const NotesScreen(),
+    );
   }
 }
