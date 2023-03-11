bb18fd11837bb417e111a08f68f92a105cb11a8c
marwan <marwan.elansarey@gmail.com>
2023-03-11 21:54:29 +0200

    model note hive packges

--- lib/model/model_note.dart
+++ lib/model/model_note.dart
@@ -1,7 +1,17 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+
+part 'model_note.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  // att. :=
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subTitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
 
   NoteModel({
