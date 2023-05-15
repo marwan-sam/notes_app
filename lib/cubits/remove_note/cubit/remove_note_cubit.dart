8f152db9a9edc7fd0841d26ed8184f3e2242ea68
marwan <marwan.elansarey@gmail.com>
2023-05-15 20:37:57 +0200

    replace

--- lib/cubits/remove_note/cubit/remove_note_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-
-part 'remove_note_state.dart';
-
-class RemoveNoteCubit extends Cubit<RemoveNoteState> {
-  RemoveNoteCubit() : super(RemoveNoteInitial());
-}
